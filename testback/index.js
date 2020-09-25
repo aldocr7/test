24cc81ccae3c0d29908ea44b2b631e8e93b1b23c
Ronaldo <m.ronaldo20798@gmail.com>
2020-09-25 20:00:34 +0530

    commit on 'master'

--- testback/index.js
+++ testback/index.js
@@ -1,13 +1,22 @@
-const express = require "express";
+const express = require ('express');
 
 const app = express();
 
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = 8000;
 
-app.listen(port, () => {
-  console.log(`Examplrone app listening at http://localhost:${port}`)
-})
+app.get("/login", (req,res) =>{
+    return res.send("You are visiting login route");
+
+});
+
+
+app.get("/signup", (req,res) =>{
+    return res.send("You are visiting sign up route");
+
+});
+
+app.listen(port , () => {
+    console.log("server is up and running...");
+});
+
